dc68aae7c8d5c907b8d1b96b321a22a74df215c9
Cássio Pereira <cassiorodp@gmail.com>
2023-03-08 09:20:10 -0300

    refactor: create code-cell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import Preview from './components/preview';
-import bundler from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const greeting = "Hello World!";'
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
