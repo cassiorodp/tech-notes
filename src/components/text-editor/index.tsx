4ef70252d23f9dbb917132f3c62b183173d62cdb
Cássio Pereira <cassiorodp@gmail.com>
2023-03-09 16:05:24 -0300

    feat: add click outside funcionality

---
+++ src/components/text-editor/index.tsx
@@ -0,0 +1,42 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useEffect, useRef, useState } from 'react';
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={editorRef}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
+    </div>
+  );
+};
+
+export default TextEditor;
